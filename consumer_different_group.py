95f3e32bc89f8ff8fd6af6f9de2b4e7609c14411
CoderInBoots <35434195+coderinboots@users.noreply.github.com>
2024-12-31 16:17:59 +0000

    Create consumer_different_group.py

---
+++ consumer_different_group.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('topic03',group_id='my-second-group',
+                         bootstrap_servers=['192.168.1.28:9092'], auto_offset_reset='earliest')
+#consumer.subscribe('topic01')
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print("%s:%d:%d: key=%s value=%s, type=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value, type(message.value)))
+
